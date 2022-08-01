6c7d10ac4ec9d094f86a9c63220f3c733e43857e
Ritin Malhotra <rm5486@nyu.edu>
2022-08-02 00:11:16 +0530

    Add Reset Button Functionality

--- EaSScript.js
+++ EaSScript.js
@@ -1,4 +1,5 @@
 const rainbowColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+let rainbowColorsMode = false;
 
 function createGrid(numRows = 16, numCols = 16) {
     let grid = document.querySelector('.grid');
@@ -8,8 +9,8 @@ function createGrid(numRows = 16, numCols = 16) {
             gridSquare.classList.add('grid-square');
             gridSquare.style.width = (grid.offsetWidth / numRows) - 2;
             gridSquare.style.height = (grid.offsetHeight / numCols);
-            console.log(gridSquare.style.width);
-            console.log(gridSquare.style.height);
+            // console.log(gridSquare.style.width);
+            // console.log(gridSquare.style.height);
             gridSquare.addEventListener('mouseover', (e) => {
                 addColor(e.target);
             });
@@ -18,8 +19,12 @@ function createGrid(numRows = 16, numCols = 16) {
     }
 }
 
-function addColor(gridSquare) {
-    gridSquare.style.backgroundColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+function reset() {
+    let grid = document.querySelector('.grid');
+    grid.childNodes.forEach(square => square.style.backgroundColor = null);
 }
 
 createGrid(16, 16);
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', reset);
